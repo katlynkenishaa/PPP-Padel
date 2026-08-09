1e0a97169d1be11c433dbc4c747a4fc850e511ae
katlynkenishaa <katlynkenisha@gmail.com>
2026-08-09 19:38:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.set_page_config(page_title="draft request menu PPP", page_icon="🎾")
+st.set_page_config(
+    page_title="draft request menu PPP", 
+    page_icon="🎾", 
+    initial_sidebar_state="expanded"
+)
+
+# Adding sidebar element forces Streamlit to render the navigation menu
+st.sidebar.title("Navigation")
+st.sidebar.info("Select a page above.")
 
 st.title("🎾 Welcome to draft request menu PPP")
 st.info("👈 Please select a tool from the sidebar menu on the left.")
